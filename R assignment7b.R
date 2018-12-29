8e5166ab1f3eab075ddf1267a47ecfcbf4586ff8
cleodinechiome <44890461+cleodinechiome@users.noreply.github.com>
2018-12-29 23:50:52 +0200

    Add files via upload

---
+++ R assignment7b.R
@@ -0,0 +1,20 @@
+# PROBLEM STATEMENT ONE
+rent = c(1550,1700,900,850,1000,950)
+sd(rent) #standard deviation of the rentals
+
+# PROBLEM STATEMENT TWO
+height = c(3, 21, 98, 203, 17, 9)
+var(height) #variance of the height
+
+# PROBLRM STATEMENT THREE
+
+# let X be the number of subjects a student from the given class has failed in.
+# THE pdf FOR THIS DISTRIBUTION IS SHOWN BELOW:
+
+#               X       P(X=x)
+#               0         0.8
+#               1         0.1
+#               2         0.07
+#               3         0.03
+
+
